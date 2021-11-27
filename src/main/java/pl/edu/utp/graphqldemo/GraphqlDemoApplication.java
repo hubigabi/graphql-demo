3f778b0c6001191e2e416d2c8c172bd432d9cb26
hubigabi <hubigabi19@gmail.com>
2021-11-27 13:12:00 +0100

    Added client

--- src/main/java/pl/edu/utp/graphqldemo/GraphqlDemoApplication.java
+++ src/main/java/pl/edu/utp/graphqldemo/GraphqlDemoApplication.java
@@ -1,13 +1,33 @@
 package pl.edu.utp.graphqldemo;
 
+import okhttp3.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class GraphqlDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GraphqlDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlDemoApplication.class, args);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void doSomethingAfterStartup() throws IOException {
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create("{\"query\":\"query {\\r\\n  customers {\\r\\n    id\\r\\n    name\\r\\n  }\\r\\n}\",\"variables\":{}}", mediaType);
+        Request request = new Request.Builder()
+                .url("http://localhost:8080/graphql")
+                .method("POST", body)
+                .addHeader("Content-Type", "application/json")
+                .build();
+        Response response = client.newCall(request).execute();
+        System.out.println(response.body().string());
+    }
 
 }
